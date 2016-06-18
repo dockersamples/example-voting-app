1a0b0c8b5424c3cc1a944902281a6a02c75cc66c
Ben Firshman <ben@firshman.co.uk>
2016-06-17 19:56:11 -0700

    Set redis socket timeout to 5s

--- voting-app/app.py
+++ voting-app/app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0)
+        g.redis = Redis(host="redis", db=0, socket_timeout=5)
     return g.redis
 
 @app.route("/", methods=['POST','GET'])
