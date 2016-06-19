ffdd4940c6197f280722f37df35a11f028279aa7
Aanand Prasad <aanand.prasad@gmail.com>
2016-06-19 11:34:14 -0700

    Relax result test

--- result/tests/tests.sh
+++ result/tests/tests.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 while ! timeout 1 bash -c "echo > /dev/tcp/vote/80"; do sleep 1; done
-curl -sS -X POST --data "vote=a" http://vote > /dev/null
 curl -sS -X POST --data "vote=b" http://vote > /dev/null
 sleep 10
-if phantomjs render.js http://result | grep -q '2 votes'; then
+if phantomjs render.js http://result | grep -q '1 vote'; then
   echo -e "\e[42m------------"
   echo -e "\e[92mTests passed"
   echo -e "\e[42m------------"
