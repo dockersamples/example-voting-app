a12b34bc205ad8ea5f5f3cdc2fa3f48b32446107
Go Chiba <go.chiba@gmail.com>
2016-09-27 12:48:14 +0900

    Change voting items to "Tab" and "Indent".

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Cattle")
-option_b = os.getenv('OPTION_B', "Pets")
+option_a = os.getenv('OPTION_A', "Tab")
+option_b = os.getenv('OPTION_B', "Indent")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
