d9cb5fb084f94651ac8693a901226430b7548f43
Go Chiba <go.chiba@gmail.com>
2016-09-27 13:40:38 +0900

    Change voting items to "Foo" and "Hoge".

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Tab")
-option_b = os.getenv('OPTION_B', "Indent")
+option_a = os.getenv('OPTION_A', "Foo")
+option_b = os.getenv('OPTION_B', "Hoge")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
