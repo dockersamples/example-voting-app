f2eac327ffd1bba6244b911a334b01e2309f3516
Chris Ciborowski <chris@ciborowski.net>
2016-09-28 22:30:34 -0700

    Fixed cat tests

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Cat")
-option_b = os.getenv('OPTION_B', "Dog")
+option_a = os.getenv('OPTION_A', "Cats")
+option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
