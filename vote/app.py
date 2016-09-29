448a84b64bf570bb552264756579c506c5317cfe
Go Chiba <go.chiba@gmail.com>
2016-09-29 16:10:42 +0900

    Revert voting items to "Cats" and "Dogs".

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Pikachu")
-option_b = os.getenv('OPTION_B', "Mario")
+option_a = os.getenv('OPTION_A', "Cats")
+option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
