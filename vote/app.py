a64b6870cda4654e720d63cffcae34b011c050fe
Go Chiba <go.chiba@gmail.com>
2017-02-07 14:33:02 +0900

    Revert to Cats & Dogs.

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Pineapple Pen")
-option_b = os.getenv('OPTION_B', "Apple Pen")
+option_a = os.getenv('OPTION_A', "Cats")
+option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
