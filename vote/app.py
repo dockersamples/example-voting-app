f4b861244a237d11981b31a8f814f67ddb32adc1
Natasza Nowakowska <natasza@fingerspitz.se>
2017-09-07 19:54:56 +0200

    Update app.py
    Change to swedish

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "KATTER")
+option_b = os.getenv('OPTION_B', "HUNDAR")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
