bd22589985bc8987e0dbce9614d02a1a80a5321c
Bret Fisher <bret@bretfisher.com>
2017-11-30 04:48:48 -0500

    add vote logging and vote-generrator

--- vote/app.py
+++ vote/app.py
@@ -27,6 +27,7 @@ def hello():
     if request.method == 'POST':
         redis = get_redis()
         vote = request.form['vote']
+        app.logger.info('Received vote for %s', vote)
         data = json.dumps({'voter_id': voter_id, 'vote': vote})
         redis.rpush('votes', data)
 
