50ca39f51b21d262535e532d1b63b6aba01614b2
Dustin Van Buskirk <dev@vanbuskirk.me>
2018-04-11 22:20:45 -0700

    missed a db connection

--- worker/src/Worker/Program.cs
+++ worker/src/Worker/Program.cs
@@ -46,7 +46,7 @@ namespace Worker
                         if (!pgsql.State.Equals(System.Data.ConnectionState.Open))
                         {
                             Console.WriteLine("Reconnecting DB");
-                            pgsql = OpenDbConnection("Server=db;Username=postgres;");
+                            pgsql = OpenDbConnection("Server=postgresql;Username=postgres;");
                         }
                         else
                         { // Normal +1 vote requested
