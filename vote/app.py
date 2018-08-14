5291bb6cf6209f3f833e439bcc8fb1a082ada71c
Laura Frank <ljfrank@gmail.com>
2018-08-14 18:33:36 +0200

    Cats vs Dogs

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Tabs")
-option_b = os.getenv('OPTION_B', "Spaces")
+option_a = os.getenv('OPTION_A', "Cats")
+option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
