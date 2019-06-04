a8aabe3c62657f8fe6155dc91a27b2c26446dc82
Gourav Shah <gs@initcron.org>
2019-06-04 10:11:57 +0000

    added mock unit tests for worker

---
+++ worker/src/test/java/worker/unitWorker.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+ 
+class WorkerUnitTest {
+ 
+    @Test
+    void sample1() {
+ 
+    }
+
+
+    @Test
+    void sample2() {
+
+    }
+
+    @Test
+    void sample3() {
+
+    }
+
+    @Test
+    void sample4() {
+
+    }
+}
