8abaec0953bcb517808defe5d019c70f1effb4b2
Gourav Shah <gs@initcron.org>
2019-06-05 20:56:59 +0530

    added unit tests for results nodejs app

---
+++ result/test/mock.test.js
@@ -0,0 +1,29 @@
+const expect = require('chai').expect;
+
+describe('mock test 1', () => {
+  it('unit test 1', () => {
+    expect(true).to.be.true;
+  });
+}); 
+
+
+describe('mock test 2', () => {
+  it('unit test 2', () => {
+    expect(true).to.be.true;
+  });
+});
+
+describe('mock test 3', () => {
+  it('unit test 3', () => {
+    expect(true).to.be.true;
+  });
+});
+
+
+describe('mock test 4', () => {
+  it('unit test 4', () => {
+    expect(true).to.be.true;
+  });
+});
+
+
