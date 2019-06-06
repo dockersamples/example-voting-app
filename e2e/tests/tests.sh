e115e2884455734d3308b9ed8f04158b5b75e762
Gourav Shah <gs@initcron.org>
2019-06-06 11:55:56 +0530

    - refactored and renamed  docker-compose for e2e tests - compose now reads list of images from .env file - refactored e2e tests script, which can now run from any starting state of vote counts - e2e tests now run with bash insteaead of sh

--- e2e/tests/tests.sh
+++ e2e/tests/tests.sh
@@ -1,13 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
 while ! timeout 1 bash -c "echo > /dev/tcp/vote/80"; do
     sleep 1
 done
 
+current=`phantomjs render.js http://result | grep -i vote | cut -d ">" -f 4 | cut -d " " -f1`
+next=`echo "$(($current + 1))"`
+
+  echo -e "\n\n-----------------"
+  echo -e "Current Votes Count: $current"
+  echo -e "-----------------\n"
+
+echo -e " I: Submitting one more vote...\n"
+
 curl -sS -X POST --data "vote=b" http://vote > /dev/null
 sleep 10
 
-if phantomjs render.js http://result | grep -q '1 vote'; then
+new=`phantomjs render.js http://result | grep -i vote | cut -d ">" -f 4 | cut -d " " -f1`
+
+
+  echo -e "\n\n-----------------"
+  echo -e "New Votes Count: $new"
+  echo -e "-----------------\n"
+
+echo -e "I: Checking if votes tally......\n"
+
+if [ "$next" -eq "$new" ]; then
   echo -e "\\e[42m------------"
   echo -e "\\e[92mTests passed"
   echo -e "\\e[42m------------"
