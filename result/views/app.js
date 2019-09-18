149c3ac10afd1911cf42f5b77772ae8f76529386
Ramneek Khurana <ramkhur@amazon.com>
2019-09-18 13:13:48 +0800

    updated

--- result/views/app.js
+++ result/views/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('NUSVSNTU', []);
+var app = angular.module('catsvsdogs', []);
 var socket = io.connect({transports:['polling']});
 
 var bg1 = document.getElementById('background-stats-1');
