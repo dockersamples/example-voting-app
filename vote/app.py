fd53f396007b6fc36ffd81d08d68eebd9cb3a476
Tommy McClung <tmcclung@gmail.com>
2019-10-31 08:46:18 -0700

    cats and dogs

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Accept to YC")
-option_b = os.getenv('OPTION_B', "Reject :(")
+option_a = os.getenv('OPTION_A', "Cats")
+option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
