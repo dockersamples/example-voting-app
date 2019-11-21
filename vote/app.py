c078b1e788765797734fd00e38d7e6263dda68fc
Tommy McClung <tmcclung@gmail.com>
2019-11-21 09:32:44 -0800

    leopards or cheetahs

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Birds")
-option_b = os.getenv('OPTION_B', "Lizards")
+option_a = os.getenv('OPTION_A', "Leopards")
+option_b = os.getenv('OPTION_B', "Cheetahs")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
