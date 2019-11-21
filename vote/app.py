73a0971600d6aca791519b7ab12237d262f5d945
Tommy McClung <tmcclung@gmail.com>
2019-11-21 09:57:51 -0800

    bears again?

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Scoot")
-option_b = os.getenv('OPTION_B', "Tim")
+option_a = os.getenv('OPTION_A', "City Bears")
+option_b = os.getenv('OPTION_B', "Country Bears")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
