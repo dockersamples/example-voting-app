de6cb9df1ee55003e2298306e7b471e59941c0a1
Tommy McClung <tmcclung@gmail.com>
2019-11-23 22:23:51 -0800

    donkeys

--- vote/app.py
+++ vote/app.py
@@ -5,7 +5,7 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Bears")
+option_a = os.getenv('OPTION_A', "Dokeys")
 option_b = os.getenv('OPTION_B', "Lions")
 hostname = socket.gethostname()
 
