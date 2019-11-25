54e979f11f20a80f1998eb1247d5aef967bf4b17
Tommy McClung <tmcclung@gmail.com>
2019-11-25 15:54:12 -0800

    tim v scoot

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Dokeys")
-option_b = os.getenv('OPTION_B', "Lions")
+option_a = os.getenv('OPTION_A', "Tim")
+option_b = os.getenv('OPTION_B', "Scoot")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
