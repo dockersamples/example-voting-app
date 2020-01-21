07605067d83eb992dad4b6bd0f6baf6eaeac03a9
David Giffin <david@giffin.org>
2020-01-20 20:02:23 -0800

    Inner city vs metro

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "City Bears")
-option_b = os.getenv('OPTION_B', "Country Bears")
+option_a = os.getenv('OPTION_A', "Inner-City Bears")
+option_b = os.getenv('OPTION_B', "Metro Bears")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
