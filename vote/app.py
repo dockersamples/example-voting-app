4432fff979adcc44f81cf5eae1328f95b6f0cd62
David Giffin <david@giffin.org>
2020-05-18 15:10:17 -0700

    New build

--- vote/app.py
+++ vote/app.py
@@ -6,7 +6,7 @@ import random
 import json
 
 option_a = os.getenv('OPTION_A', "Fred 1")
-option_b = os.getenv('OPTION_B', "Craft 1")
+option_b = os.getenv('OPTION_B', "Savage 1")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
