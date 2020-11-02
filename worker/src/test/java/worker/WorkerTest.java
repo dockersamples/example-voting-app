69ddec17714668d558b8af14cc0bce92be6c88e2
Milena <milelucero_09@hotmail.com>
2020-11-02 19:10:42 -0300

    Added Worker test and tests for Vote

---
+++ worker/src/test/java/worker/WorkerTest.java
@@ -0,0 +1,25 @@
+package worker;
+
+import static org.junit.jupiter.api.Assertions.*;
+class WorkerTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void updateVote() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void connectToRedis() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void connectToDB() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sleep() {
+    }
+}
