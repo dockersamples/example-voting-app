d80b132c1830a42f0d61f2fb37541528cccab06c
Adam Vollrath <adam.d.vollrath@gmail.com>
2021-07-26 15:58:32 -0500

    Connect to localhost redis.

--- vote/app.py
+++ vote/app.py
@@ -18,7 +18,8 @@ app.logger.setLevel(logging.INFO)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0, socket_timeout=5)
+        #g.redis = Redis(host="redis", db=0, socket_timeout=5)
+        g.redis = Redis(host="localhost", db=0, socket_timeout=5)
     return g.redis
 
 @app.route("/", methods=['POST','GET'])
