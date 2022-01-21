3fa1c65fde3afd47e63b687e81c92f6faa5d0715
Julien Cartigny (TeamGrow.Info) <julien.cartigny@teamgrow.info>
2022-01-21 10:25:27 +0100

    add REDIS_HOST var env support in vote

--- vote/app.py
+++ vote/app.py
@@ -8,6 +8,7 @@ import logging
 
 option_a = os.getenv('OPTION_A', "Cats")
 option_b = os.getenv('OPTION_B', "Dogs")
+redis_host = os.getenv('REDIS_HOST', "redis")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
@@ -18,7 +19,7 @@ app.logger.setLevel(logging.INFO)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0, socket_timeout=5)
+        g.redis = Redis(host=redis_host, db=0, socket_timeout=5)
     return g.redis
 
 @app.route("/", methods=['POST','GET'])
