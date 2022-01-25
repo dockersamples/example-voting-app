e5c0867d87b1cae1b7b36e39d9b4553431122802
Yagnesh M. Bhadiyadra <35532869+yagneshmb@users.noreply.github.com>
2022-01-25 22:07:13 +0530

    Get only total marks from server.js

--- result/views/app.js
+++ result/views/app.js
@@ -5,24 +5,25 @@ var bg1 = document.getElementById('background-stats-1');
 var bg2 = document.getElementById('background-stats-2');
 
 app.controller('statsCtrl', function($scope){
-  $scope.aPercent = 50;
-  $scope.bPercent = 50;
+  //$scope.aPercent = 50;
+  //$scope.bPercent = 50;
 
   var updateScores = function(){
     socket.on('scores', function (json) {
        data = JSON.parse(json);
-       var a = parseInt(data.a || 0);
-       var b = parseInt(data.b || 0);
+       data = parseFloat(data);
+       //var a = parseInt(data.a || 0);
+       //var b = parseInt(data.b || 0);
 
-       var percentages = getPercentages(a, b);
+       //var percentages = getPercentages(a, b);
 
-       bg1.style.width = percentages.a + "%";
-       bg2.style.width = percentages.b + "%";
+       //bg1.style.width = percentages.a + "%";
+       //bg2.style.width = percentages.b + "%";
 
        $scope.$apply(function () {
-         $scope.aPercent = percentages.a;
-         $scope.bPercent = percentages.b;
-         $scope.total = a + b;
+         //$scope.aPercent = percentages.a;
+         //$scope.bPercent = percentages.b;
+         $scope.total = data;
        });
     });
   };
