a6a41b76a184be70a621fbae619d2f341c629b0c
eric <ericegan@live.com>
2022-04-21 19:43:16 -0700

    added back hyphen because dind and jenkins containers have docker-compose

--- vote/integration_test.sh
+++ vote/integration_test.sh
@@ -4,27 +4,27 @@ cd integration
 
 echo "I: Creating environment to run  integration tests..."
 
-docker compose build
-docker compose up -d
+docker-compose build
+docker-compose up -d
 
 
 echo "I: Launching Integration Test ..."
 
-docker compose run --rm integration /test/test.sh
+docker-compose run --rm integration /test/test.sh
 
 if [ $? -eq 0 ]
 then
   echo "---------------------------------------"
   echo "INTEGRATION TESTS PASSED....."
   echo "---------------------------------------"
-  docker compose down
+  docker-compose down
   cd ..
   exit 0
 else
   echo "---------------------------------------"
   echo "INTEGRATION TESTS FAILED....."
   echo "---------------------------------------"
-  docker compose down
+  docker-compose down
   cd ..
   exit 1
 fi
