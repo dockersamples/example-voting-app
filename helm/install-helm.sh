0366e9379ac07be059ecf5f066a07c9a80493bdc
luizcarloskazuyukifukaya <luizcarloskazuyukifukaya@gmail.com>
2023-04-04 17:10:42 +0900

    call another shell from a shell..

--- helm/install-helm.sh
+++ helm/install-helm.sh
@@ -1,5 +1,23 @@
 #helm install --dry-run woven-challenge helm -n challenge
 #kubectl create namespace challenge
+
+# Check previous installation
+PREVIOUS_INSTALLED=$(helm list | wc -l)
+echo $PREVIOUS_INSTALLED
+
+if [ $PREVIOUS_INSTALLED -eq "1" ]
+then
+
+  #helm install woven-challenge . -n challenge
+  echo "No previous Helm Chart installed..."
+
+else
+
+  # Uninstall previous Helm Chart
+  helm uninstall woven-challenge
+
+fi
+
 #helm install woven-challenge . -n challenge
 helm install woven-challenge .
 
