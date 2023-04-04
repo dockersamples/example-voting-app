9994c2c4c574cbe91c3682ba2f7bf82c39136d8d
luizcarloskazuyukifukaya <luizcarloskazuyukifukaya@gmail.com>
2023-04-04 16:38:17 +0900

    updated jenkinsfile

--- helm/install-helm.sh
+++ helm/install-helm.sh
@@ -1,9 +1,12 @@
 #helm install --dry-run woven-challenge helm -n challenge
-kubectl create namespace challenge
-helm install woven-challenge . -n challenge
+#kubectl create namespace challenge
+#helm install woven-challenge . -n challenge
+helm install woven-challenge .
 
 # list installed helm chart
-helm list -n challenge
+#helm list -n challenge
+helm list 
 
 # Uninstall helm chart
 # helm uninstall woven-challenge -n challenge
+# helm uninstall woven-challenge
