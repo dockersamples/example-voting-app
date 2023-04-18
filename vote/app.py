f424bcfdaddc8e304d690a8578081a9b8a3fce3e
Chris Barker <github@mrz.angrydome.com>
2023-04-18 11:13:36 +0100

    Add gitpod.yml and change to star

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "Star Trek")
+option_b = os.getenv('OPTION_B', "StarWars")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
