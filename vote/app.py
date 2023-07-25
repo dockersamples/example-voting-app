2cc63e54a785539ec51d1c2826066c98b29140a2
Sean Carolan 🍊 <sean@gitpod.io>
2023-07-25 15:27:19 -0400

    Update URLs for images.

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Star Trek")
-option_b = os.getenv('OPTION_B', "StarWars")
+option_a = os.getenv('OPTION_A', "🖖🏼 Star Trek")
+option_b = os.getenv('OPTION_B', "⚔️ Star Wars")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
