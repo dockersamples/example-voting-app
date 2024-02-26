691470ca5cfad7256f6e7890287ef47964c80630
Marcos Lilljedahl <marcosnils@gmail.com>
2024-02-25 23:06:22 -0300

    change result service to use long pollig by default
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

--- go/result/static/app.js
+++ go/result/static/app.js
@@ -1,53 +1,51 @@
-var app = angular.module('catsvsdogs', []);
+var app = angular.module("catsvsdogs", []);
 console.log("Location Host: " + location.host);
 var socket = io.connect("http://" + location.host, {
-  transports: ['websocket'],
-  path: '/socket.io/'
+  transports: ["polling", "websocket"],
+  path: "/socket.io/",
 });
 
+var bg1 = document.getElementById("background-stats-1");
+var bg2 = document.getElementById("background-stats-2");
 
-
-var bg1 = document.getElementById('background-stats-1');
-var bg2 = document.getElementById('background-stats-2');
-
-app.controller('statsCtrl', function($scope){
+app.controller("statsCtrl", function ($scope) {
   $scope.aPercent = 50;
   $scope.bPercent = 50;
-  var updateScores = function(){
-    socket.on('scores', function (json) {
+  var updateScores = function () {
+    socket.on("scores", function (json) {
       console.log("JSON update scores: " + json);
-       data = JSON.parse(json);
-       console.log("Getting scores" + data)
-       var a = parseInt(data.a || 0);
-       var b = parseInt(data.b || 0);
+      data = JSON.parse(json);
+      console.log("Getting scores" + data);
+      var a = parseInt(data.a || 0);
+      var b = parseInt(data.b || 0);
 
-       var percentages = getPercentages(a, b);
+      var percentages = getPercentages(a, b);
 
-       bg1.style.width = percentages.a + "%";
-       bg2.style.width = percentages.b + "%";
+      bg1.style.width = percentages.a + "%";
+      bg2.style.width = percentages.b + "%";
 
-       $scope.$apply(function () {
-         $scope.aPercent = percentages.a;
-         $scope.bPercent = percentages.b;
-         $scope.total = a + b;
-       });
+      $scope.$apply(function () {
+        $scope.aPercent = percentages.a;
+        $scope.bPercent = percentages.b;
+        $scope.total = a + b;
+      });
     });
   };
 
-  var init = function(){
-    document.body.style.opacity=1;
+  var init = function () {
+    document.body.style.opacity = 1;
     updateScores();
   };
-  socket.on("connect",function(data){
+  socket.on("connect", function (data) {
     init();
-  } )
+  });
 });
 
 function getPercentages(a, b) {
   var result = {};
 
   if (a + b > 0) {
-    result.a = Math.round(a / (a + b) * 100);
+    result.a = Math.round((a / (a + b)) * 100);
     result.b = 100 - result.a;
   } else {
     result.a = result.b = 50;
