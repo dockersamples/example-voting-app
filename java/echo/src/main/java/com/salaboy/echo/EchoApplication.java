b5acece3a49e25c186736fb0bc6b4abf4d31a33d
salaboy <Salaboy@gmail.com>
2024-03-04 10:33:16 +0000

    adding websockets and jsconfetti

--- java/echo/src/main/java/com/salaboy/echo/EchoApplication.java
+++ java/echo/src/main/java/com/salaboy/echo/EchoApplication.java
@@ -1,23 +1,24 @@
 package com.salaboy.echo;
 
-
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.web.codec.CodecCustomizer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.codec.CodecConfigurer;
-
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import io.cloudevents.CloudEvent;
 import io.cloudevents.spring.webflux.CloudEventHttpMessageReader;
 import io.cloudevents.spring.webflux.CloudEventHttpMessageWriter;
-
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -29,31 +30,52 @@ public class EchoApplication {
 
 }
 
-
 @RestController
 @RequestMapping("/")
 class EchoController {
 
 	private static final Logger logger = LoggerFactory.getLogger(EchoController.class);
 
+	@Value("${PUBLIC_IP:localhost}")
+	private String publicIp;
+
+	private final SimpMessagingTemplate simpMessagingTemplate;
 
 	@PostMapping("events")
-	CloudEvent echo(@RequestBody CloudEvent cloudEvent)  {
+	CloudEvent echo(@RequestBody CloudEvent cloudEvent) {
 
 		logger.info("Echo CloudEvent: " + cloudEvent.toString());
+		emitWSEvent(cloudEvent);
 
 		return cloudEvent;
+	}
+
+	public EchoController(SimpMessagingTemplate simpMessagingTemplate) {
+		this.simpMessagingTemplate = simpMessagingTemplate;
 	}
 
 	@Configuration
 	class CloudEventHandlerConfiguration implements CodecCustomizer {
 
-    @Override
-    public void customize(CodecConfigurer configurer) {
-        configurer.customCodecs().register(new CloudEventHttpMessageReader());
-        configurer.customCodecs().register(new CloudEventHttpMessageWriter());
-    }
+		@Override
+		public void customize(CodecConfigurer configurer) {
+			configurer.customCodecs().register(new CloudEventHttpMessageReader());
+			configurer.customCodecs().register(new CloudEventHttpMessageWriter());
+		}
+	}
 
-}
+	@GetMapping("/server-info")
+	public Info getInfo() {
+		return new Info(publicIp);
+	}
+
+	public record Info(String publicIp) {
+	}
+
+	private void emitWSEvent(CloudEvent event) {
+		System.out.println("Emitting Event via WS: " + event.toString());
+		simpMessagingTemplate.convertAndSend("/topic/events",
+				event);
+	}
 
 }
---
+++ java/echo/src/main/resources/static/app.js
@@ -0,0 +1,69 @@
+const stompClient = new StompJs.Client({
+});
+
+const jsConfetti = new JSConfetti()
+
+function connect() {
+
+  console.log("Fetching Server Info")
+  fetch("/server-info", {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  }).then((response) => {
+    console.log("Fetching Response")
+    return response.json();
+  }).then((response) => {
+    var publicURL = 'ws://' + response.publicIp + '/ws';
+    stompClient.brokerURL = publicURL;
+    console.log(publicURL);
+    console.log("Activating client")
+    stompClient.activate();
+  }).catch((error) => {
+    console.error(`Could not get server-info: ${error}`);
+  });
+};
+
+stompClient.onConnect = (frame) => {
+
+  console.log('Connected: ' + frame);
+  stompClient.subscribe('/topic/events', (event) => {
+    console.log(JSON.parse(event.body));
+    showEvent(JSON.parse(event.body));
+
+  });
+};
+
+function showEvent(event) {
+  console.log("Option Selected: " + event.data.node.option)
+  if (event.data.node.option == "a") {
+    jsConfetti.addConfetti({
+      emojis: ['😺'],
+      emojiSize: 100,
+      confettiNumber: 30,
+    })
+  }
+  if (event.data.node.option == "b") {
+    jsConfetti.addConfetti({
+      emojis: ['🐶'],
+      emojiSize: 100,
+      confettiNumber: 30,
+    })
+  }
+
+}
+
+
+
+
+stompClient.onWebSocketError = (error) => {
+  console.error('Error with websocket', error);
+};
+
+stompClient.onStompError = (frame) => {
+  console.error('Broker reported error: ' + frame.headers['message']);
+  console.error('Additional details: ' + frame.body);
+};
+
+
