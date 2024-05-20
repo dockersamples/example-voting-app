7b8827b566cbdc1e0dfeb75b178745c76944dac8
root <root@ip-172-31-38-132.us-east-2.compute.internal>
2024-05-20 11:04:16 +0000

    Cricketrs are updated

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "Virat Koholi")
+option_b = os.getenv('OPTION_B', "M S Dhoni")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
