c19a787b6f891e61760919799065d902c0773887
Sathyavani <rsathyavani@gmail.com>
2024-07-02 18:45:45 +0530

    Changed Choices

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Coffee")
-option_b = os.getenv('OPTION_B', "Tea")
+option_a = os.getenv('OPTION_A', "Cats")
+option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
